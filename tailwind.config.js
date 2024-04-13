a302c5ba270cf1f44ce6982b9f5df4b9ba997227
blueempire-design <163209268+blueempire-design@users.noreply.github.com>
2024-04-13 03:10:57 -0300

    Home Page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+        colors: {
+        primary: 'rgba(255, 255, 255, 0.25)',
+        primaryWhite: '#ffffff', // Este será el color que usaremos cuando hagas scroll
+      },
+    },
+  },
+  plugins: [],
+}
+
