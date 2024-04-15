d94cc3a053e5695e7c8225b3806574f44d0f6f75
blueempire-design <blueempiredesign@gmail.com>
2024-04-15 16:58:51 -0300

    Agregue un Topbar info

---
+++ app.js
@@ -0,0 +1,24 @@
+  const boton = document.querySelector('#boton');
+    const menu = document.querySelector('#menu');
+
+    boton.addEventListener('click', () => {
+      console.log('click')
+      menu.classList.toggle('hidden')
+    })
+ 
+
+  window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('nav');
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100) { // Cambia 100 por la posición en la que deseas que ocurra el cambio de color
+    navbar.classList.remove('bg-primary');
+    navbar.classList.add('bg-primaryWhite');
+  } else {
+    navbar.classList.remove('bg-primaryWhite');
+    navbar.classList.add('bg-primary');
+  }
+});
+
+
+ 
